b75f8107f9289e8722fcc34d999d7dfedda54187
Kyle Wymer <wymerkd@gmail.com>
2019-03-06 20:19:30 -0800

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
 };
 
-$(document).ready(function()  {
+$(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
