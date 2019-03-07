ee81acab68e6f991f0b75aa7fa6ae52840f5aee1
Kyle Wymer <wymerkd@gmail.com>
2019-03-06 19:59:02 -0800

    add single evaluation that checks whethether the year is divisible by 4 or 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
